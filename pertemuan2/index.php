a84e35ebbeec6aef0705d735b9a21f61fe47d217
farzyahoo <76583066+farzyahoo@users.noreply.github.com>
2021-05-20 13:20:57 +0700

    Add files via upload

---
+++ pertemuan2/index.php
@@ -0,0 +1,86 @@
+<?php
+/* 
+    Farriz Brilliant Wichaksana
+    203040177
+    github.com/Farzyahoo/pw2021_203040177
+    Pertemuan 2( 6 Februari 2021 )
+    Materi Minggu ini mempelajari mengenai penggunaan sintaks PHP Dasar 
+*/
+?>
+
+
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+// echo "Farriz Brilliant Wichkansana";
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh di awali angka, tapi boleh mengandung angka
+// $nama = "Farriz";
+
+// echo "Halo, nama saya $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// &y = 20;
+// echo $x = $y;
+
+// penggabung sting / concatenation / concat.
+// $nama_depan = "Farriz";
+// $nama_belakang = "Wichaksana";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment
+// =, +=, -=, *=, /=, %=, .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Farriz";
+// $nama .= " ";
+// $nama .= "Wichaksana";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 30;
+// var_dump($x < 20 || $x % 2 == 0)
+
+?>
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1>Halo, Selamat datang <?php echo "$nama"; ?></h1>
+    <p><?php echo "Ini adalah paragraf"; ?></p>
+</body>
+</html> -->
+
+    <!-- <?php
+     echo "<h1>Halo, Selamat Datang Farriz</h1>" 
+     ?> -->

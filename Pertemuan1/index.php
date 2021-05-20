1b213c3ebdc47db40b808e6db0fc9d22ac1b974b
farzyahoo <76583066+farzyahoo@users.noreply.github.com>
2021-05-20 13:16:35 +0700

    Add files via upload

---
+++ Pertemuan1/index.php
@@ -0,0 +1,11 @@
+<?php
+    echo "Hello Word";
+?>
+
+/* 
+    Farriz Brilliant Wichaksana
+    203040177
+    github.com/Faryahoo/pw2021_203040177
+    Pertemuan 2( 6 Februari 2021 )
+    Materi Minggu ini tentang Instalasi VCS, XAMPP
+*/

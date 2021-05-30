bc4100dc530434237ce19a826bf70119a3820483
farzyahoo <76583066+farzyahoo@users.noreply.github.com>
2021-05-30 15:12:27 +0700

    Add files via upload

---
+++ Pertemuan13/ajax/ajax.php
@@ -0,0 +1,43 @@
+<?php
+require '../functions.php';
+$mahasiswa = cari($_GET['keyword']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>Nama</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php if (empty($mahasiswa)) : ?>
+      <tr>
+        <td colspan="4">
+          <p style="color: red; font-style: italic;">data mahasiswa tidak ditemukan!</p>
+        </td>
+      </tr>
+    <?php endif; ?>
+
+    <?php $i = 1;
+    foreach ($mahasiswa as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $m['gambar']; ?>" width="60"></td>
+        <td><?= $m['nama']; ?></td>
+        <td>
+          <a href="detail.php?id=<?= $m['id']; ?>">lihat detail</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
